ff36a29c79d11a78ef005f78dd1756d60891b782
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-09 21:33:52 -0700

    initial push

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,8 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig");
+
+function findById() {
+  console.log("hello world");
+}
+
+module.exports = { findById };
--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,8 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+async function findById() {
+  console.log("hello world");
+}
+
+module.exports = { findById };
