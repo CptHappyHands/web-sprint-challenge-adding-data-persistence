b93ded4cd49842089cecac6732c22504e1088340
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-10 10:54:19 -0700

    15 of 18 tests passing

--- api/task/model.js
+++ api/task/model.js
@@ -1,8 +1,30 @@
 // build your `Task` model here
 const db = require("../../data/dbConfig");
 
-async function findById() {
-  console.log("hello world");
+async function find() {
+  const tasksResults = await db("tasks");
+
+  const tasksInfo = tasksResults.map((e) => {
+    return {
+      tasks_id: e.tasks_id,
+      tasks_notes: e.tasks_notes,
+      tasks_description: e.tasks_description,
+      tasks_completed: e.tasks_completed === 1 ? true : false,
+    };
+  });
+
+  return tasksInfo;
 }
 
-module.exports = { findById };
+async function add(tasks) {
+  const [id] = await db("tasks").insert(tasks);
+  const newTasks = {
+    tasks_id: id,
+    tasks_notes: tasks.tasks_notes,
+    tasks_description: tasks.tasks_description,
+    tasks_completed: tasks.tasks_completed === 1 ? true : false,
+  };
+  return newTasks;
+}
+
+module.exports = { find, add };
--- api/task/router.js
+++ api/task/router.js
@@ -4,8 +4,22 @@ const Tasks = require("./model");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {});
+router.get("/", (req, res, next) => {
+  Tasks.find()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
 
-router.post("/", (req, res, next) => {});
+router.post("/", (req, res, next) => {
+  const tasks = req.body;
+
+  Tasks.add(tasks)
+    .then((task) => {
+      res.status(201).json(task);
+    })
+    .catch(next);
+});
 
 module.exports = router;
