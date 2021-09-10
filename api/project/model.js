b6d09f8746476de45f9d924dd8d1636b55043247
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-10 10:38:29 -0700

    passing 10 of 18 tests

--- api/project/model.js
+++ api/project/model.js
@@ -2,28 +2,35 @@
 const db = require("../../data/dbConfig");
 
 async function find() {
-  const projectResults = await db("projects as p");
+  // return await db("projects");
+  const projectResults = await db("projects");
 
   const projectInfo = projectResults.map((e) => {
-    return [
-      {
-        project_id: e.project_id,
-        project_name: e.project_name,
-        project_description: e.project_description,
-        project_completed: e.project_completed,
-      },
-    ];
+    return {
+      project_id: e.project_id,
+      project_name: e.project_name,
+      project_description: e.project_description,
+      project_completed: e.project_completed === 1 ? true : false,
+    };
   });
 
   return projectInfo;
 }
 
-function add(project) {
-  return db("projects")
-    .insert(project)
-    .then(([project_id]) => {
-      return db("projects").where("project_id", project_id).first();
-    });
+async function add(project) {
+  const [id] = await db("projects").insert(project);
+  const newProject = {
+    project_id: id,
+    project_name: project.project_name,
+    project_description: project.project_description,
+    project_completed: project.project_completed === 1 ? true : false,
+  };
+  return newProject;
+  // return db("projects")
+  //   .insert(project)
+  //   .then(([project_id]) => {
+  //     return db("projects").where("project_id", project_id).first();
+  //   });
   // const newProject = {
   //   project_id: id,
   //   project_name
