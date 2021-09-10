2a6342643a888a8667d78729679a7ef8c62e4818
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-10 08:29:11 -0700

    initial migration done

---
+++ data/migrations/20210910151528_initial-migration.js
@@ -0,0 +1,43 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (table) => {
+      table.increments("project_id");
+      table.string("project_name").notNullable();
+      table.string("project_description");
+      table.integer("project_completed");
+    })
+    .createTable("resources", (table) => {
+      table.increments("resource_id");
+      table.string("resource_name").notNullable().unique();
+      table.string("resource_description");
+    })
+    .createTable("tasks", (table) => {
+      table.increments("task_id");
+      table.string("task_description").notNullable();
+      table.string("task_notes");
+      table.integer("task_completed");
+      table
+        .integer("project_id")
+        .notNullable()
+        .unsigned()
+        .references("project_id")
+        .inTable("projects");
+    })
+    .createTable("project_resources", (table) => {
+      table.increments("project_resources_id");
+      table
+        .integer("resource_id")
+        .notNullable()
+        .unsigned()
+        .references("resource_id")
+        .inTable("resources");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
